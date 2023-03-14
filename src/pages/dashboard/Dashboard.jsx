2f33135537751d445747225ddc53ba0d4bd23c84
kadjialan <alankadji7@gmail.com>
2023-03-14 21:46:46 +0100

    sidebar design

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -9,22 +9,90 @@ function Dashboard() {
           <span>
             <i className="fa-solid fa-user" />
             <div className="identifier">
-              <h3>kadji alan</h3>
+              <p>
+                <b>kadji alan</b>
+              </p>
               <p>alankadji@gmail.com</p>
             </div>
           </span>
         </div>
+        <div className="content">
+          <h2>Categories</h2>
+          <ul>
+            <li>
+              <input type="checkbox" />
+              Beer
+            </li>
+            <li>
+              <input type="checkbox" />
+              Champagne
+            </li>
+            <li>
+              <input type="checkbox" />
+              Whisky
+            </li>
+            <li>
+              <input type="checkbox" />
+              Juice
+            </li>
+            <li>
+              <input type="checkbox" />
+              Water
+            </li>
+            <li>
+              <input type="checkbox" />
+              Tea
+            </li>
+          </ul>
 
-        <h3>categories</h3>
-        <ul>
-          <li>
-            <input type="checkbox" />
-            Beer
-          </li>
-          <li>champagne</li>
-          <li>whisky</li>
-          <li>juice</li>
-        </ul>
+          <h2>Glasses</h2>
+          <ul>
+            <li>
+              <input type="checkbox" />
+              Tall
+            </li>
+            <li>
+              <input type="checkbox" />
+              Medium
+            </li>
+            <li>
+              <input type="checkbox" />
+              small
+            </li>
+          </ul>
+
+          <h2>Ingredients</h2>
+          <ul>
+            <li>
+              <input type="checkbox" />
+              Mais
+            </li>
+            <li>
+              <input type="checkbox" />
+              Champagne
+            </li>
+            <li>
+              <input type="checkbox" />
+              Whisky
+            </li>
+            <li>
+              <input type="checkbox" />
+              Juice
+            </li>
+            <li>
+              <input type="checkbox" />
+              Water
+            </li>
+            <li>
+              <input type="checkbox" />
+              Tea
+            </li>
+          </ul>
+          <div className="end">
+            <i className="fa-solid fa-right-from-bracket" />
+            <p>log out</p>
+          </div>
+        </div>
       </div>
     </div>
   );

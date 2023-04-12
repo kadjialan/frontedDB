ed8ac01e51b57e6e3c3ee2bea3e69fa023c1f794
kadjialan <alankadji7@gmail.com>
2023-04-12 23:33:21 +0100

    implementation of table pagination

--- src/pages/Actions/AddIngredients/AddIngredients.jsx
+++ src/pages/Actions/AddIngredients/AddIngredients.jsx
@@ -2,14 +2,24 @@
 import React, { useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate';
 import { findIngredients } from '../../../api/auth';
+import { httpClient } from '../../../api/axios';
 import './Addingredients.css';
 
 function AddIngredients() {
   const [ingredient, setIngredient] = useState();
+  const [data1, setData1] = useState();
+  const [currentPage, setcurrentPage] = useState(1);
+  const [limit, setLimit] = useState(1);
 
   function handlePageClick(e) {
     console.log(e);
+    httpClient
+      .get(`ingredients/${currentPage}&${limit}`)
+      .then(({ data }) => setData1(data));
+    console.log(data1);
   }
+
+  // `http://localhost:3000/ingredients/${currentPage}&${limit}`
 
   useEffect(() => {
     findIngredients().then(setIngredient);
@@ -27,7 +37,7 @@ function AddIngredients() {
           </tr>
         </thead>
         <tbody>
-          {ingredient?.map((rum, index) => (
+          {/*  {ingredient?.map((rum, index) => (
             <tr key={rum?.id}>
               <td> {index + 1} </td>
               <td> {rum?.name} </td>
@@ -38,7 +48,7 @@ function AddIngredients() {
                 <i className="fa-solid fa-trash-can" />
               </td>
             </tr>
-          ))}
+          ))} */}
         </tbody>
       </table>
       <ReactPaginate
@@ -46,9 +56,18 @@ function AddIngredients() {
         nextLabel="next >"
         onPageChange={handlePageClick}
         pageRangeDisplayed={5}
-        pageCount={5}
+        pageCount={8}
         previousLabel="< previous"
         renderOnZeroPageCount={null}
+        marginPagesDisplayed={2}
+        containerClassName="pagination justify-content-center"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        activeClassName="active"
       />
     </div>
   );
